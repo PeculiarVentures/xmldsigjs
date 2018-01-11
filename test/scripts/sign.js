119333fb1b5375b07f8aa66b5df37ef74d6cc11d
microshine <microshine@mail.ru>
2018-01-11 21:05:13 +0300

    Add tests

--- test/scripts/sign.js
+++ test/scripts/sign.js
@@ -39,6 +39,7 @@ if (typeof module !== "undefined" && typeof exports !== "undefined") {
 
         function checkXMLSEC(xml) {
             return new Promise((resolve, reject) => {
+                // console.log("%s\n", xml);
                 fs.writeFileSync(SIGN_XML_FILE, xml, { flag: "w+" });
 
                 child_process.exec(`xmlsec1 verify ${SIGN_XML_FILE}`, (err, stdout) => {
@@ -51,34 +52,70 @@ if (typeof module !== "undefined" && typeof exports !== "undefined") {
             })
         }
 
-        it("Simple", (done) => {
-            (async () => {
+        context("Enveloped", () => {
+            function test(xml, id, done) {
+                (async () => {
+                    let signedXml = new xmldsig.SignedXml();
+                    const xmlDocument = xmldsig.Parse(xml);
 
-                var xml = xmldsig.Parse(`<root><first/><second/></root>`);
-                let signedXml = new xmldsig.SignedXml();
-
-
-                const signature = await signedXml.Sign(alg, keys.privateKey, xml, {
-                    keyValue: keys.publicKey,
-                    references: [
+                    const signature = await signedXml.Sign(
+                        alg,
+                        keys.privateKey,
+                        xmlDocument,
                         {
-                            hash: "SHA-256",
-                            transforms: ["enveloped"],
-                        }
-                    ],
-                });
+                            keyValue: keys.publicKey,
+                            references: [
+                                {
+                                    hash: "SHA-256",
+                                    transforms: ["enveloped"],
+                                    id,
+                                }
+                            ],
+                        });
 
-                xml.documentElement.appendChild(signature.GetXml());
+                    xmlDocument.documentElement.appendChild(signature.GetXml());
 
-                // serialize XML
-                const oSerializer = new XMLSerializer();
-                const sXML = oSerializer.serializeToString(xml);
-                // console.log(sXML.toString())
-                
+                    // serialize XML
+                    const oSerializer = new XMLSerializer();
+                    const sXML = oSerializer.serializeToString(xmlDocument);
 
-                await checkXMLSEC(sXML.toString());
+                    await checkXMLSEC(sXML);
 
-            })().then(done, done);
+                    //#region Verify
+                    const vXml = xmldsig.Parse(sXML);
+                    const vSignature = vXml.getElementsByTagNameNS("http://www.w3.org/2000/09/xmldsig#", "Signature")[0];
+                    const verifyXml = new xmldsig.SignedXml(vXml);
+                    verifyXml.LoadXml(vSignature);
+                    const ok = await verifyXml.Verify();
+                    assert.equal(ok, true);
+                    //#endregion
+
+                })().then(done, done);
+            }
+
+            it("without namespace", (done) => {
+                test(`<root><first/><second/></root>`, null, done);
+            });
+
+            it("with default root namespace", (done) => {
+                test(`<root xmlns="http://namespace1"><first/><second/></root>`, null, done);
+            });
+
+            it("with root namespaces", (done) => {
+                test(`<root xmlns="http://namespace1" xmlns:ns2="http://namespace2"><ns3:first xmlns:ns3="http://namespace3"/><ns2:second/></root>`, null, done);
+            });
+
+            it("child without namespace", (done) => {
+                test(`<root xmlns="http://namespace1"><first id="id-1"/><second/></root>`, "id-1", done);
+            });
+
+            it("child with namespace", (done) => {
+                test(`<root xmlns="http://namespace1"><ns2:first id="id-1" xmlns:ns2="http://namespace2"/><second/></root>`, "id-1", done);
+            });
+
+            it("child with repeated namespace", (done) => {
+                test(`<root xmlns="http://namespace1" xmlns:ns2="http://namespace3"><ns2:first id="id-1" xmlns:ns2="http://namespace2"/><ns2:second/></root>`, "id-1", done);
+            });
         });
 
     });
