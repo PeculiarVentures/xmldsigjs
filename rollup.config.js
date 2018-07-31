58945f978853ce05454f593a9c6466069b0946f0
microshine <microshine@mail.ru>
2018-07-31 13:09:13 +0300

    Switch to esnext

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,4 @@
 import typescript from "rollup-plugin-typescript";
-import babel from "rollup-plugin-babel";
-import babelrc from "babelrc-rollup";
 
 let pkg = require("./package.json");
 let external = Object.keys(pkg.dependencies);
@@ -11,8 +9,7 @@ let sourceMap = process.argv.some(item => item.toLowerCase() === "--dev");
 export default {
     input: "src/index.ts",
     plugins: [
-        typescript({ typescript: require("typescript"), target: "es5" }),
-        babel(babelrc()),
+        typescript({ typescript: require("typescript") })
     ],
     external: external,
     output: [
