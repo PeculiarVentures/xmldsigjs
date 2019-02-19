80645d14257bda09ed77eeba28debca102148adc
Xinxin Li <xinxin.li@sympli.com.au>
2019-02-19 11:17:49 +1100

    another try

---
+++ rollup.config.es5.js
@@ -0,0 +1,23 @@
+import nodeResolve from "rollup-plugin-node-resolve";
+import typescript from "rollup-plugin-typescript";
+
+let pkg = require("./package.json");
+let external = Object.keys(pkg.dependencies);
+
+let sourceMap = process.argv.some(item => item.toLowerCase() === "--dev");
+
+export default {
+    input: "src/index.ts",
+    plugins: [
+        typescript({ typescript: require("typescript"), target: "es5" }),
+        nodeResolve({ jsnext: true, main: true }),
+    ],
+    output: [
+        {
+            file: "dist/index.js",
+            format: "umd",
+            name: "XmlDSigJs",
+            sourceMap
+        }
+    ]
+};
