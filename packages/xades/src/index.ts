817ce98203047159da59371c9b1ca55453946157
sosoba <2843525+sosoba@users.noreply.github.com>
2025-09-24 13:31:28 +0200

    reexport setNodeDependencies

--- packages/xades/src/index.ts
+++ packages/xades/src/index.ts
@@ -2,4 +2,4 @@ export * as xml from './xml/index.js';
 export * from './signed_xml.js';
 
 export { Application } from 'xmldsigjs';
-export { Select, Parse, Convert } from 'xml-core';
+export { setNodeDependencies, Select, Parse, Convert } from 'xml-core';
