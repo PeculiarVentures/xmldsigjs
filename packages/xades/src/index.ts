48b1b8f3430915fe72f6592cd0b0be6e0fb6eb8a
microshine <microshine@mail.ru>
2025-12-15 21:43:50 +0100

    style: format exports

--- packages/xades/src/index.ts
+++ packages/xades/src/index.ts
@@ -2,4 +2,6 @@ export * as xml from './xml/index.js';
 export * from './signed_xml.js';
 
 export { Application } from 'xmldsigjs';
-export { Select, Parse, Convert, setNodeDependencies } from 'xml-core';
+export {
+  Select, Parse, Convert, setNodeDependencies,
+} from 'xml-core';
